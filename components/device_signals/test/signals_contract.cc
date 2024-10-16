703ff99060f3eb258b72a54b585ce38831648681
Graeme Jenkinson <graeme@capabilitieslimited.co.uk>
2024-10-16 15:21:15 +0100

    Correct conditional from the FreeBSD port.

--- components/device_signals/test/signals_contract.cc
+++ components/device_signals/test/signals_contract.cc
@@ -186,7 +186,7 @@ GetSignalsContract() {
       base::BindRepeating(VerifyUnset, names::kCrowdStrike);
 #endif
 
-#if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX) || BUIDLFLAG(IS_BSD)
+#if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_BSD)
   contract[names::kAllowScreenLock] =
       base::BindRepeating(VerifyUnset, names::kAllowScreenLock);
   contract[names::kImei] = base::BindRepeating(VerifyUnset, names::kImei);
