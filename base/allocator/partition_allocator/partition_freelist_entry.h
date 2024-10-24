435d0ebd09c14968caefda1066ecfe11723e6972
Graeme Jenkinson <graeme@capabilitieslimited.co.uk>
2024-10-24 11:46:00 +0100

    [cheri] Dummy encode the freelist entry address.
    On CHERI architectures encoding the address by reversing the bytes
    results in an invalid capability. As the encoding is designed to protect
    the address on the freelist from access through buffer overflow attacks,
    it is of quesrtionable benefit on CHERI architectures.
    
    Latter versions of partition alloc provide an alternative encoding based
    on offsets. Although this looks like it could be adapted for CHERI
    architectures, as the value of the approach is currently questionable
    this is left for a future stage where the security of partition alloc is
    investigated.

--- base/allocator/partition_allocator/partition_freelist_entry.h
+++ base/allocator/partition_allocator/partition_freelist_entry.h
@@ -72,12 +72,22 @@ class EncodedPartitionFreelistEntryPtr {
     //    corrupt a freelist pointer, partial pointer overwrite attacks are
     //    thwarted.
     // For big endian, similar guarantees are arrived at with a negation.
+#if defined(__CHERI_PURE_CAPABILITY__)
+    // FIXME(gcjenkinson): The encoding scheme invalidates the capability.
+    // Later versions of parition_alloc use a difference encoding scheme based
+    // on offsets, which looks like it is more suited to CHERI architectures.
+    // For now, a null encoding is provided. This will be reviewed as part of
+    // the wider taks of securing partition_alloc, for example, in narrowing
+    // allocation bounds.
+    return address;
+#else   // !__CHERI_PURE_CAPABILITY__)
 #if defined(ARCH_CPU_BIG_ENDIAN)
     uintptr_t transformed = ~address;
 #else
     uintptr_t transformed = ReverseBytes(address);
 #endif
     return transformed;
+#endif   // !__CHERI_PURE_CAPABILITY__
   }
 
   uintptr_t encoded_;
