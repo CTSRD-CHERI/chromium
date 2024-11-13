28417a3bdb9f1abbd457fef974a0a06359fcfad4
Graeme Jenkinson <graeme@capabilitieslimited.co.uk>
2024-11-13 15:02:29 +0000

    Fix IsResident for BSD platforms.
    Remove the conditional for IS_BSD returning false, and correct the call
    mincore for BSD platforms. This fixes a number of failing unittests.

--- base/memory/madv_free_discardable_memory_posix.cc
+++ base/memory/madv_free_discardable_memory_posix.cc
@@ -28,6 +28,10 @@
 #include <sys/prctl.h>
 #endif
 
+#if BUILDFLAG(IS_BSD)
+#include <sys/mman.h>
+#endif
+
 #if BUILDFLAG(ENABLE_BASE_TRACING)
 #include "base/trace_event/memory_allocator_dump.h"  // no-presubmit-check
 #include "base/trace_event/memory_dump_manager.h"    // no-presubmit-check
@@ -306,11 +310,9 @@ void MadvFreeDiscardableMemoryPosix::SetKeepMemoryForTesting(bool keep_memory) {
 
 bool MadvFreeDiscardableMemoryPosix::IsResident() const {
   DFAKE_SCOPED_RECURSIVE_LOCK(thread_collision_warner_);
-// XXX mincore
-#if BUILDFLAG(IS_BSD)
-  return false;
-#else
 #if BUILDFLAG(IS_APPLE)
+  std::vector<char> vec(allocated_pages_);
+#elif BUILDFLAG(IS_BSD)
   std::vector<char> vec(allocated_pages_);
 #else
   std::vector<unsigned char> vec(allocated_pages_);
@@ -325,7 +327,6 @@ bool MadvFreeDiscardableMemoryPosix::IsResident() const {
       return false;
   }
   return true;
-#endif
 }
 
 bool MadvFreeDiscardableMemoryPosix::IsDiscarded() const {
