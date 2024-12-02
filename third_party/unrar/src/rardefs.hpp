ded85fb1420650053fd64c75714f31699dbb93c5
Graeme Jenkinson <graeme@capabilitieslimited.co.uk>
2024-12-02 15:46:09 +0000

    [cheri] Use __builtin_align_up in RAR when present.
    Use __builtin_align_up to preserve capability provenance n alignment
    calculations.

--- third_party/unrar/src/rardefs.hpp
+++ third_party/unrar/src/rardefs.hpp
@@ -50,7 +50,11 @@
 #endif
 
 // Produce the value, which is equal or larger than 'v' and aligned to 'a'.
+#if __has_builtin(__builtin_align_up)
+#define ALIGN_VALUE(v,a) __builtin_align_up(v, a)
+#else
 #define ALIGN_VALUE(v,a) (size_t(v) + ( (~size_t(v) + 1) & (a - 1) ) )
+#endif
 
 #if defined(_WIN_ALL) && !defined(SFX_MODULE)
 #define PROPAGATE_MOTW // Propagate the archive Mark of the Web.
