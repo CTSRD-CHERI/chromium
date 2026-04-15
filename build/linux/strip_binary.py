e1565de821a9e9ee6abfc9f6dc4e395ffb905950
Graeme Jenkinson <graeme@capabilitieslimited.co.uk>
2026-04-15 11:19:59 +0100

    Apply patches from Chromium FreeBSD port
    Apply build patches from Chromium 145.0.7632.159 FreeBSD port.

--- build/linux/strip_binary.py
+++ build/linux/strip_binary.py
@@ -10,6 +10,7 @@ import sys
 
 
 def main() -> int:
+  return 0
   parser = argparse.ArgumentParser(description="Strip binary using LLVM tools.")
   parser.add_argument("--llvm-strip-binary-path",
                       help="Path to llvm-strip executable.")
