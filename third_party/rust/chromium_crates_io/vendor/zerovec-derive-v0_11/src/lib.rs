96516a2355327618677ba7addd5d958dc099d7c3
Graeme Jenkinson <graeme@capabilitieslimited.co.uk>
2026-07-15 16:03:30 +0100

    [cheri] Enable unstable features for zerovec-derive
    Enable unstable features lint_reasons and add to -Zallow-features

--- third_party/rust/chromium_crates_io/vendor/zerovec-derive-v0_11/src/lib.rs
+++ third_party/rust/chromium_crates_io/vendor/zerovec-derive-v0_11/src/lib.rs
@@ -2,6 +2,8 @@
 // called LICENSE at the top level of the ICU4X source tree
 // (online at: https://github.com/unicode-org/icu4x/blob/main/LICENSE ).
 
+#![feature(lint_reasons)]
+
 //! Proc macros for generating `ULE`, `VarULE` impls and types for the `zerovec` crate
 
 use proc_macro::TokenStream;
