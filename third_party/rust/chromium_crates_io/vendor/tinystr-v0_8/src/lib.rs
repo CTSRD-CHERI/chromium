56cc25a564f11b18e850bb94f5e8ee50815c7297
Graeme Jenkinson <graeme@capabilitieslimited.co.uk>
2026-07-17 10:39:54 +0100

    [cheri] Add support for Rust 1.72 tinystr
    Summary of changes:
    
    - Enable unstable features error_in_core and lint_reasons and add to
      -Zallow-features when compiling with the Morello Rust compiler
      (v1.72). error_in_core and lint_reasons where stabilised in version
      1.81.

--- third_party/rust/chromium_crates_io/vendor/tinystr-v0_8/src/lib.rs
+++ third_party/rust/chromium_crates_io/vendor/tinystr-v0_8/src/lib.rs
@@ -51,6 +51,9 @@
 //!
 //! [`ICU4X`]: ../icu/index.html
 
+#![feature(cfg_version)]
+#![cfg_attr(not(version("1.81")), feature(error_in_core, lint_reasons))]
+
 // https://github.com/unicode-org/icu4x/blob/main/documents/process/boilerplate.md#library-annotations
 #![cfg_attr(not(any(test, doc)), no_std)]
 #![cfg_attr(
