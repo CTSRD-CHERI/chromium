998ab370090b0bd6c1e4a8efd5eb2c819d909663
Graeme Jenkinson <graeme@capabilitieslimited.co.uk>
2026-09-10 16:21:19 +0100

    Replace use of C++23 #elifdef

--- base/threading/platform_thread_linux_base.cc
+++ base/threading/platform_thread_linux_base.cc
@@ -13,7 +13,7 @@ size_t GetDefaultThreadStackSize(const pthread_attr_t& attributes) {
   // ThreadSanitizer bloats the stack heavily. Evidence has been that the
   // default stack size isn't enough for some browser tests.
   return 2 * (1 << 23);  // 2 times 8192K (the default stack size on Linux).
-#elifdef __GLIBC__
+#elif defined(__GLIBC__)
   // Generally glibc sets ample default stack sizes, so use the default there.
   return 0;
 #else
